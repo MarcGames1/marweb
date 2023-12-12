a6432610a0bd00be2324c8e2a3d6d4072ff750b5
MarcGames1 <George.marcu20@gmail.com>
2023-12-12 19:35:07 +0200

    updated redirect

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = { async redirects() {
     return [
       {
         source: '/cpanel',
-        destination: 'https://89.42.218.12:2083',
+        destination: 'https://server-0391.whmpanels.com:2083/',
         permanent: true,
       },
     ];}}
