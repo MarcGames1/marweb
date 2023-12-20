91d29e0d0f20e4d8947c6a146118c04d8a292674
MarcGames1 <George.marcu20@gmail.com>
2023-12-20 13:21:35 +0200

    x

--- frontend/src/components/Header/HeaderComponents/Menu/index.tsx
+++ frontend/src/components/Header/HeaderComponents/Menu/index.tsx
@@ -1,9 +1,143 @@
-import React from 'react'
+'use client'
+import { motion, useCycle } from 'framer-motion';
 
-const Menu = () => {
+
+
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: 'circle(30px at 40px 40px)',
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+export const Menu = () => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  
+//   const { height } = useDimensions(containerRef);
+
   return (
-    <div>index</div>
-  )
+    <motion.nav onClick={()=>{toggleOpen()}} className=' cursor-pointer'
+      initial={false}
+      animate={isOpen ? 'opened' : 'closed'}
+   
+     
+    >
+      <MenuButton isOpen={isOpen} width={24} height={24} strokeWidth={2 } color={'#fff'} transition={undefined} lineProps={undefined} />
+      <motion.div className="background" variants={sidebar} />
+      
+    </motion.nav>
+  );
+};
+
+
+interface props {
+  isOpen: boolean;
+  width: number;
+  height: number;
+  strokeWidth: number;
+  color: string;
+  transition: any;
+  lineProps: any;
 }
 
-export default Menu
+const MenuButton = ({
+  isOpen = false,
+  width = 24,
+  height = 24,
+  strokeWidth = 1,
+  color = "#000",
+  transition = null,
+  lineProps = null,
+  ...props
+}: props) =>{
+const variant = isOpen ? "opened" : "closed";
+  const top = {
+    closed: {
+      rotate: 0,
+      translateY: 0
+    },
+    opened: {
+      rotate: 45,
+      translateY: 2
+    }
+  };
+  const center = {
+    closed: {
+      opacity: 1
+    },
+    opened: {
+      opacity: 0
+    }
+  };
+  const bottom = {
+    closed: {
+      rotate: 0,
+      translateY: 0
+    },
+    opened: {
+      rotate: -45,
+      translateY: -2
+    }
+  };
+  lineProps = {
+    stroke: color,
+    strokeWidth: strokeWidth as number,
+    vectorEffect: "non-scaling-stroke",
+    initial: "closed",
+    animate: variant,
+    transition,
+    ...lineProps
+  };
+  const unitHeight = 4;
+  const unitWidth = (unitHeight * (width as number)) / (height as number);
+
+  return (
+    <motion.svg
+      
+      viewBox={`0 0 ${unitWidth} ${unitHeight}`}
+      overflow="visible"
+      preserveAspectRatio="none"
+      width={width}
+      height={height}
+      {...props}
+    >
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="0"
+        y2="0"
+        variants={top}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="2"
+        y2="2"
+        variants={center}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0"
+        x2={unitWidth}
+        y1="4"
+        y2="4"
+        variants={bottom}
+        {...lineProps}
+      />
+    </motion.svg>
+  );
+};
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Logo from './HeaderComponents/Logo'
-import Menu from './HeaderComponents/Menu'
-
+import { Menu } from './HeaderComponents/Menu'
 
 const Header = () => {
-  return (<header className='sticky inset-0 z-10 glass'>
+  return (<header className='sticky inset-0 z-10 glass flex  justify-between pr-20 items-center'>
 
     <Logo />
     <Menu />
