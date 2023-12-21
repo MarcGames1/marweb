ed909d5b4459b109da42b42ae50627720f06d76d
MarcGames1 <George.marcu20@gmail.com>
2023-12-21 13:43:39 +0200

    Update DesktopMenu.tsx

--- frontend/src/components/Header/DesktopMenu.tsx
+++ frontend/src/components/Header/DesktopMenu.tsx
@@ -12,24 +12,23 @@ const DesktopMenu = () => {
     
     if(isMobile){return}
   return (
-    <header className=" hidden sticky inset-0 z-10 bg-transparent lg:flex backdrop-blur-lg shadow-2xl items-center justify-center	 px-2 ">
+    <header className=" hidden  sticky backdrop-contrast-100 inset-0 z-10 bg-transparent lg:flex backdrop-blur-lg shadow-2xl items-center justify-center   px-2 ">
       <Logo />
-      <nav className="flex  flex-nowrap justify-evenly   navbar  ">
-        <ul className=" bg-transparent menu-horizontal gap-4   px-1">
+      <nav className="flex mix-blend-overlay bg-inherit backdrop-contrast-200 flex-nowrap justify-evenly   navbar  ">
+        <ul className="  menu-horizontal gap-4   px-1">
           {MenuData.map((menu, idx) => {
             return (
               <li key={idx}>
                 <MenuItem menu={menu} />
               </li>
-            ); 
+            );
           })}
         </ul>
-      <div className='flex gap-4 items-center justify-self-end'>
-      <Buttons.Whatsapp />
-      <Buttons.Calendly />
-      </div>
+        <div className="flex gap-4 items-center justify-self-end">
+          <Buttons.Whatsapp />
+          <Buttons.Calendly />
+        </div>
       </nav>
-
     </header>
   );
 }
