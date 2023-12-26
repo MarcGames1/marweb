7243ef9c58763bc3d526032f0a8bea3b0e7811de
MarcGames1 <George.marcu20@gmail.com>
2023-12-26 11:25:57 +0200

    Update styles.ts

--- frontend/src/date/styles.ts
+++ frontend/src/date/styles.ts
@@ -1,4 +1,4 @@
 export const typography = {
-  h1: 'text-center mix-blend-difference block m-auto text-7xl text-primary-content',
-  h2: 'text-center  block m-auto  text-primary-content mix-blend-difference text-5xl',
+  h1: 'text-center  block m-auto text-7xl text-primary-content',
+  h2: 'text-center  block m-auto  text-primary-content  text-5xl',
 };
