3215a9f03cadd8e598bc7ddcaf8c0ca3032041b4
MarcGames1 <George.marcu20@gmail.com>
2023-12-28 16:25:59 +0200

    Update Catsvg.tsx

--- frontend/src/components/CatFace/Catsvg.tsx
+++ frontend/src/components/CatFace/Catsvg.tsx
@@ -5,7 +5,11 @@ import React, {
   MouseEvent as ReactMouseEvent,
 } from 'react';
 
-const CatFace: React.FC = () => {
+const CatFace: React.FC = ({
+  fillColor = '#233144',
+}: {
+  fillColor?: string;
+}) => {
   const [eyePosition, setEyePosition] = useState<{ x: number; y: number }>({
     x: 0,
     y: 0,
@@ -21,13 +25,14 @@ const CatFace: React.FC = () => {
   };
 
   useEffect(() => {
-    const catFace = document.getElementById('cat-face');
+    const catFace = document.querySelector('#cat-face');
+    const doc = document.querySelector('html');
     if (catFace) {
       // @ts-ignore
-      catFace.addEventListener('mousemove', updateEyePosition);
+      doc.addEventListener('mousemove', updateEyePosition);
       return () => {
         // @ts-ignore
-        catFace.removeEventListener('mousemove', updateEyePosition);
+        doc.removeEventListener('mousemove', updateEyePosition);
       };
     }
   }, []);
@@ -39,16 +44,15 @@ const CatFace: React.FC = () => {
       height="200"
       viewBox="0 0 200 200"
       xmlns="http://www.w3.org/2000/svg"
-      
     >
       {/* Face */}
-      <circle cx="100" cy="100" r="80" fill="black" />
+      <circle cx="100" cy="100" r="80" fill={fillColor} />
 
       {/* Left ear */}
-      <polygon points="50,20 80,80 50,80"  fill="black" />
+      <polygon points="50,20 80,80 50,80" fill={fillColor} />
 
       {/* Right ear */}
-      <polygon points="150,20 120,80 150,80" fill="black" />
+      <polygon points="150,20 120,80 150,80" fill={fillColor} />
 
       {/* Left eye */}
       <circle cx="70" cy="80" r="15" fill="white" />
@@ -56,7 +60,7 @@ const CatFace: React.FC = () => {
         cx={eyePosition.x < 70 ? 65 : 75}
         cy={eyePosition.y < 80 ? 75 : 85}
         r="5"
-        fill="black"
+        fill={fillColor}
       />
 
       {/* Right eye */}
@@ -65,7 +69,7 @@ const CatFace: React.FC = () => {
         cx={eyePosition.x > 130 ? 135 : 125}
         cy={eyePosition.y < 80 ? 75 : 85}
         r="5"
-        fill="black"
+        fill={fillColor}
       />
       {/* Nose */}
       <circle cx="100" cy="110" r="5" fill="pink" />
@@ -78,9 +82,6 @@ const CatFace: React.FC = () => {
       <line x1="115" y1="120" x2="130" y2="120" stroke="white" />
       <line x1="85" y1="130" x2="70" y2="140" stroke="white" />
       <line x1="115" y1="130" x2="130" y2="140" stroke="white" />
-
-      
-      
     </svg>
   );
 };
