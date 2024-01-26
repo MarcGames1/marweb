63d3e5528ae2eafc518e72c6d57802e690c71c3c
MarcGames1 <George.marcu20@gmail.com>
2024-01-27 00:59:30 +0200

    menu

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,31 +1,54 @@
 'use client'
 import { MenuData } from '@/date/menuData';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence, useCycle} from 'framer-motion';
 import Link from 'next/link';
 import { useState } from 'react';
 import { Buttons } from '..';
 import Logo from './HeaderComponents/Logo';
-
+ 
 export const Navbar = () => {
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!active);
   };
- const variants = {
-   opened: { x: 0, opacity: 1, display: 'flex' },
-   closed: { x: -500, display: 'none', opacity: 0 },
- };
-  return (
-    <>
-      <nav
-        className={`w-full z-30 w-auto flex items-center flex-wrap glass p-3 `}
-      >
-        <div className="inline-flex items-center p-2 mr-4 md:w-[100px]">
-          <Logo />
-        </div>
+  const hideNavItemsVariant = {
+    opened: {
+      opacity: 0,
+      y: '-100%',
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+    closed: {
+      opacity: 1,
+      y: '0%',
+      transition: {
+        delay: 1.1,
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  };
 
-        <button
+
+
+  return (
+    <AnimatePresence>
+      <motion.nav
+        initial="closed"
+        animate={active ? 'opened' : 'closed'}
+        className={`${active ? 'absolute top-0': ''} w-full z-30 w-auto flex items-center flex-wrap glass p-3 `}
+      >
+        <motion.div
+          variants={hideNavItemsVariant}
+          className="inline-flex items-center p-2 mr-4 md:w-[100px]"
+        >
+          <Logo />
+        </motion.div>
+
+        <motion.button
           className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
           onClick={handleClick}
         >
@@ -43,20 +66,25 @@ export const Navbar = () => {
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
-        </button>
+        </motion.button>
         {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-        <div
+        <motion.div 
+          
           className={`${
             active ? '' : 'hidden'
-          }    w-full lg:inline-flex text-center  lg:flex-grow lg:w-auto`}
+          }    w-full lg:inline-flex text-center lg:translate-y-0  lg:flex-grow lg:w-auto`}
         >
-          <div className="lg:inline-flex md:gap-2 gap-5 lg:flex-row lg:ml-auto lg:w-auto lg:items-center items-start  flex flex-col  items-center">
+          <motion.div
+           
+            className="lg:inline-flex md:gap-2 gap-5 lg:flex-row lg:ml-auto lg:w-auto lg:items-center items-start  flex flex-col  items-center"
+          >
             {MenuData.map((m, i) => {
               return (
                 <motion.div
+                  whileTap={{ scale: 0.95 }}
                   className="lg:inline-flex md:w-auto lg:w-auto w-full   px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white  text-center"
                   //@ts-ignore
-                  variants={variants}
+
                   key={i}
                 >
                   <Link href={m.href}>{m.text}</Link>
@@ -67,9 +95,9 @@ export const Navbar = () => {
               <Buttons.Calendly />
               <Buttons.Whatsapp />
             </div>
-          </div>
-        </div>
-      </nav>
-    </>
+          </motion.div>
+        </motion.div>
+      </motion.nav>
+    </AnimatePresence>
   );
 };
