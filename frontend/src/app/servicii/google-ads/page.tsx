02ae0c6702cad3ef880486e360fcb84eb1a2beb8
MarcGames1 <George.marcu20@gmail.com>
2024-01-28 23:32:29 +0200

    xx

--- frontend/src/app/servicii/google-ads/page.tsx
+++ frontend/src/app/servicii/google-ads/page.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Section1 from './sections/Section1'
 import GoogleAdsData from '@/date/googleAds'
+import { Section2 } from './sections/Section2';
 
 const GoogleAdsPage = () => {
   return (
     <>
       <Section1 src="/assets/ADS/hero.png" {...GoogleAdsData.S1} />
+      <Section2  {...GoogleAdsData.S2} />
     </>
   );
 }
+
+
 
 export default GoogleAdsPage
---
+++ frontend/src/app/servicii/google-ads/sections/Section2.tsx
@@ -0,0 +1,6 @@
+import { SectionWithCards } from "@/date/types";
+
+export const Section2 = ({heading, content }:SectionWithCards) => {
+
+    return <></>
+};
