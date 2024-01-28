a9d2df4e9f6eb27bc85140ee1155bfc3e1f5c77f
MarcGames1 <George.marcu20@gmail.com>
2024-01-28 20:34:36 +0200

    Update Section3.tsx

--- frontend/src/app/servicii/seo/Sections/Section3.tsx
+++ frontend/src/app/servicii/seo/Sections/Section3.tsx
@@ -12,28 +12,7 @@ const Section3 = ({heading, content}:SectionWithCardsAndPic) => {
   )
 }
 
-const Rightside = () =>{
-return (
-  <>
-    <div className=" sticky top-12 items-center justify-center place-content-center place-items-center place-self-center ">
-      <div className="flex flex-col mx-5 p-5  rounded-2xl outline_hover justify-self-center place-self-center">
-        <H className='pb-5' size={TextSize.xl2} level={'span'}>Contacteaza-ma</H>
-        <Image
-          alt="iphone-mockup"
-          width={400}
-          height={400}
-          src={'/assets/DespreMine/Poza-Alexandru-Marcu.png'}
-        />
-        <br />
-        <div className="flex flex-row justify-around">
-          <Buttons.Calendly />
-          <Buttons.Whatsapp />
-        </div>
-      </div>
-    </div>
-  </>
-);
-}
+
 
 const LeftSide = ({ content, heading }: SectionWithCards) => {
   return (
