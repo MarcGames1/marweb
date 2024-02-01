ae8f03b161591135c504007ca4fc92a6d1f152a6
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 01:58:53 +0200

    Update VideoControl.tsx

--- frontend/src/sections/heroVideo/VideoControl.tsx
+++ frontend/src/sections/heroVideo/VideoControl.tsx
@@ -11,7 +11,7 @@ const VideoControl = ({isMuted, setIsMuted}:React.ComponentState) => {
       onClick={(e) => {
         setIsMuted(!isMuted);
       }}
-      className="rounded-full   shadow-2xl bg-white absolute bottom-1 right-10 flex items-center bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l p-3 text-lg text-white mt-6"
+      className="rounded-full right-0   shadow-2xl bg-white absolute bottom-1  flex items-center bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l p-3 text-lg text-white mt-6"
     >
       {isMuted ? <FaVolumeOff size={'2em'} /> : <FaVolumeUp size={'2em'} />}
     </button>
