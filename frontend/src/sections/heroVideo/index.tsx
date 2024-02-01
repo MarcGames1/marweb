3ef859a0dcc64fc43d5d48023a4743b4084bd119
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 01:56:59 +0200

    Update index.tsx

--- frontend/src/sections/heroVideo/index.tsx
+++ frontend/src/sections/heroVideo/index.tsx
@@ -8,7 +8,7 @@ import VideoClientComponent from './VideoClientComponent';
 const HeroFullScreenVideo = () => {
   
   return (
-    <div className="max-w-screen min-h-[50vh]  lg:min-h-screen relative">
+    <div className="max-w-full min-h-[30vh] lg:min-h-screen relative">
     <VideoClientComponent />
   </div>
   );
