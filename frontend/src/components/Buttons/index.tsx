61e3de8f8bf847e4e8a139f135b7096447fa183f
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 11:40:05 +0200

    Update index.tsx

--- frontend/src/components/Buttons/index.tsx
+++ frontend/src/components/Buttons/index.tsx
@@ -7,11 +7,9 @@ const Whatsapp = () => {
       role="button"
       href={'https://wa.me/+40750472447'}
       title="Scrie-mi un mesaj pe Whatsapp"
-      className=" flex items-center bg-gradient-to-r from-color-820 to-color-50  duration-200 transition ease-linear hover:bg-gradient-to-l px-4 py-3 text-lg text-white rounded-[35px] mt-6"
+      className=" flex items-center justify-items-center bg-gradient-to-r from-color-820 to-color-50  duration-200 transition ease-linear hover:bg-gradient-to-l px-4 py-3 lg:text-lg text-white rounded-[35px] mt-6 max-h-[3rem] w-auto break-keep	"
     >
-      <FaWhatsapp />
-      &nbsp;
-      Whatsapp
+      <FaWhatsapp size={'1.5em'} />&nbsp;Whatsapp
     </Link>
   );
 }
@@ -22,9 +20,9 @@ const Calendly = () => {
         role="button"
         title="Programeaza un call gratuit"
         href={'https://calendly.com/alexandru-marcu/consultanta-gratuita'}
-        className=" flex items-center bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l px-4 py-3 text-lg text-white rounded-[35px] mt-6"
+        className=" flex items-center justify-items-center bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l px-4 py-3 lg:text-lg text-white rounded-[35px] mt-6 max-h-[3rem]  w-auto break-keep	"
       >
-        <FaRegCalendarAlt /> &nbsp; Programeaza-te!
+        <FaRegCalendarAlt size={'1.5em'} />&nbsp;Programeaza-te
       </Link>
     );
 }
