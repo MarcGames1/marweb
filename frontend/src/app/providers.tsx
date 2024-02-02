051d831f804c97180910838a4a08e905ae243151
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 02:21:07 +0200

    xx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,17 +1,12 @@
 'use client'
 import { ThemeProvider } from 'next-themes';
-import { ReactNode, useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
 import { ToastContainer } from 'react-toastify';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-   useEffect(() => {
-     AOS.init({ duration: 1200, disable: 'mobile' });
-     AOS.refresh();
-   }, []);
+
   return (
-    <div data-aos="zoom-in">
+    <div>
       <ToastContainer
         position="top-right"
         autoClose={2000}
