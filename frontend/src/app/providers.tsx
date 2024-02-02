24d7011ec10da479c640a8f4d5d3e89d6827a6a7
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 02:16:35 +0200

    Update providers.tsx

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -7,11 +7,11 @@ import { ToastContainer } from 'react-toastify';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
    useEffect(() => {
-     AOS.init({ duration: 1200 });
+     AOS.init({ duration: 1200, disable: 'mobile' });
      AOS.refresh();
    }, []);
   return (
-    <div data-aos='fade'>
+    <div data-aos="zoom-in">
       <ToastContainer
         position="top-right"
         autoClose={2000}
