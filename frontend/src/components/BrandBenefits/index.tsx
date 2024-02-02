8b83df9de2cf8743b4b99c4313b1779328cf1dcf
MarcGames1 <George.marcu20@gmail.com>
2024-02-02 11:56:58 +0200

    Update index.tsx

--- frontend/src/components/BrandBenefits/index.tsx
+++ frontend/src/components/BrandBenefits/index.tsx
@@ -4,27 +4,27 @@ import styles from './BrandBenefits.module.css'
 import { TextColor, TextSize } from '../Typography/H';
 import { HiAcademicCap } from 'react-icons/hi';
 import { TbBrandReactNative, TbSeo } from 'react-icons/tb';
+import { CardBGColor } from '@/date/types';
 
 
 const BrandBenefits = () => {
   return (
-    <section className="bg-gradient-to-b from-[#2A0B70] to-[#0C0A27]   ">
-      
+    <section className="   ">
       <div className="container flex lg:flex-row m-auto  flex-col items-center justify-evenly gap-5 lg:gap-0 p-5  h-fit">
         <BenefitsCard
           beneficiu="Expertiză Remarcabilă în SEO și Google Ads"
           image={styles.image1}
-          Icon={<TbSeo size={'3em'} color={'#C619A4'} />}
+          Icon={<TbSeo size={'3em'} color={'#FA5252'} />}
         />
         <BenefitsCard
           beneficiu="Soluții Personalizate de Dezvoltare Website"
-          Icon={<TbBrandReactNative size={'3em'} color={'#C619A4'} />}
+          Icon={<TbBrandReactNative size={'3em'} color={'#FA5252'} />}
           image={styles.image2}
         />
         <BenefitsCard
           beneficiu="Sesiuni de Mentorat Unu la Unu și Experiență Extinsă"
           image={styles.image3}
-          Icon={<HiAcademicCap size={'3em'} color={'#C619A4'} />}
+          Icon={<HiAcademicCap size={'3em'} color={'#FA5252'} />}
         />
       </div>
     </section>
@@ -40,14 +40,14 @@ interface IBenefitsCard {
 const BenefitsCard = ({ image, beneficiu, Icon }: IBenefitsCard) => {
     
   return (
-    <div className="relative shrink-0 outline_hover w-[300px] h-[200px]   rounded-2xl ">
+    <div className="relative shrink-0  w-[300px] h-[200px]   rounded-2xl ">
       <div
-        className={`${image} mix-blend-luminosity w-[300px] h-[200px] rounded-2xl outline_hover  absolute  inset-0	`}
+        className={`${image}  w-[300px] h-[200px] rounded-2xl   absolute  inset-0	`}
       ></div>
       <div className="absolute top-1 left-1 mix-blend-normal ">
         {Icon}
       </div>
-      <div className="absolute bottom-0 bg-black/90 rounded-2xl p-2">
+      <div className="absolute bottom-0 bg-color-500/90 dark:bg-black/90 rounded-2xl p-2">
         {' '}
         <H
           level={'div'}
