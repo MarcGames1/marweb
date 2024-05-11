f933d6c82cab7dee70dd55325363208c62c5c18c
MarcGames1 <George.marcu20@gmail.com>
2024-05-11 20:14:42 +0300

    added GTM

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/custom.css';
 import { metadata as md } from '@/date/homepage';
 const inter = Inter({ subsets: ['latin'] });
 import Providers from './providers';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = md;
 
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html  lang="ro">
     <head>
       <link rel="icon" href="/favicon.ico" sizes="any"/>
+      <GoogleTagManager gtmId="GTM-5FGZZ26C" />
     </head>
     <body className={`  ${inter.className}`}>
     <Providers>
