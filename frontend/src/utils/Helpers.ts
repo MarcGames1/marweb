7416f23ba1c3b3da06b0d7b7a595559e3447be18
MarcGames1 <George.marcu20@gmail.com>
2024-06-05 22:01:29 +0300

    Update Helpers.ts

--- frontend/src/utils/Helpers.ts
+++ frontend/src/utils/Helpers.ts
@@ -1,8 +1,19 @@
 import { v4 as uuid } from 'uuid';
 
+const datalayer:any[] = []
+
 export function gtag() {
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push(arguments);
+
+  if(typeof window !== 'undefined'){
+    window.dataLayer = window.dataLayer || [];
+    if(datalayer.length < 1){
+      window.dataLayer.push(...datalayer)
+      datalayer.splice(0, Infinity)
+    }
+    window.dataLayer.push(arguments);
+  }
+  datalayer.push(arguments)
+
 }
 
 export const generateRandomId = () => {
