2d6b2896d1c307130f6e792cc37ee74d92b5fb2f
MarcGames1 <George.marcu20@gmail.com>
2024-06-11 23:29:40 +0300

    Update sitemap.ts

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -2,11 +2,14 @@ import {MetadataRoute} from 'next';
 import {HeaderMenuData} from '@/date/menuData'
 import { getAllPostsURL } from '@/lib/mdx';
 const base_url = 'https://marweb.ro'
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): { lastModified: Date; priority: number; changeFrequency: string; url: string }[] {
 
   const urls:string[] = [base_url]
-  const posts = await getAllPostsURL()
-  posts.forEach(slug=> urls.push(`${base_url}/blog/${slug}`))
+  const data =  getAllPostsURL()
+    .then(posts =>{
+      posts.forEach(slug=> urls.push(`${base_url}/blog/${slug}`))
+
+    })
   const pagesurl = HeaderMenuData.map(menuData =>{
     return `${base_url}${menuData.routePath}`
   })
