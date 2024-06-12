b722deea8dbd66bc5b924830d816341bc773c683
MarcGames1 <George.marcu20@gmail.com>
2024-06-12 21:58:00 +0300

    Update layout.tsx

--- frontend/src/app/portofoliu/layout.tsx
+++ frontend/src/app/portofoliu/layout.tsx
@@ -3,24 +3,13 @@ import Container from '@/components/Container';
 import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 import Header from "@/components/Header";
-import { getAllPortfolioItems } from '@/lib/mdx';
 
 export const metadata: Metadata = new SEOInfo(
   'Portofoliu - MarWeb',
   'Portofoliu - MarWeb',
   '/portofoliu/'
 );
-export async function generateStaticParams() {
-  const posts = await getAllPortfolioItems()
 
-  return posts.map((post) => {
-    console.log(post.slug)
-
-    return {
-      item: encodeURIComponent(post.slug),
-    }
-  })
-}
 const PortofoliuLayout = ({ children }: { children: ReactNode }) => {
   return <>
     <Header />
