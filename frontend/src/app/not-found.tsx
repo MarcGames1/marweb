ea7da75d377d4aa888f7207334190901bbbd4bc0
MarcGames1 <George.marcu20@gmail.com>
2024-06-12 23:45:51 +0300

    Update not-found.tsx

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { Metadata } from 'next';
-import Link from 'next/link';
+import Header from '@/components/Header';
 import React from 'react'
-import { SEOInfo } from '../utils/SeoInfo';
+import { SEOInfo } from '@/utils/SeoInfo';
 import Container from '@/components/Container';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
@@ -38,23 +38,23 @@ const NotFoundPage = () => {
     console.error(error);
   } }
   return (
-    <Container>
-      <div className="h-auto w-full flex flex-col justify-center items-center  d">
-        <h1 className="text-7xl leading-none dark:text-white font-extrabold md:text-8xl">
-          404!
-        </h1>
-        <p className=" text-medium dark:text-white text-sm capitalize mb-7 mt-6 px-4 text-center">
-          Aceasta Pagina nu exista
-        </p>
+   <><Header /> <Container>
+     <div className="h-auto w-full flex flex-col justify-center items-center  d">
+       <h1 className="text-7xl leading-none dark:text-white font-extrabold md:text-8xl">
+         404!
+       </h1>
+       <p className=" text-medium dark:text-white text-sm capitalize mb-7 mt-6 px-4 text-center">
+         Aceasta Pagina nu exista
+       </p>
 
-        <button
-          onClick={handleClick}
-          className="flex items-center mx-auto bg-gradient-to-r  duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg light:text-black dark:text-white rounded-[5px]"
-        >
-          Inapoi la Pagina Principala
-        </button>
-      </div>
-    </Container>
+       <button
+         onClick={handleClick}
+         className="flex items-center mx-auto bg-gradient-to-r  duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg light:text-black dark:text-white rounded-[5px]"
+       >
+         Inapoi la Pagina Principala
+       </button>
+     </div>
+   </Container></>
   );
 };
 
